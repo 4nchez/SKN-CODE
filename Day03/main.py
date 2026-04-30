3f2ebff30b21b33a5037ffb08d2a0002c68d81d5
4nchez <4nchez@gmail.com>
2026-04-30 05:58:41 +0000

    Python basic collection

--- Day03/main.py
+++ Day03/main.py
@@ -11,9 +11,10 @@
 # import 패키지명.하위패키지명.모듈명 as 줄임말
 # import my_collections.test_list.list_sample as ls
 
+'''
 # 사용할 모듈 안의 원하는 대상만 임포트할 수도 있음
 # from 패키지명.모듈명 import 대상이름 [as 대상에 대한 줄임말]
-from my_collections.test_list.list_sample import make_list1, make_list2
+from my_collections.test_list.list_sample import make_list1, make_list2, list_indexing, list_slicing, change_element
 
 if __name__=='__main__':
     # 파이썬의 main 시그니처임
@@ -25,7 +26,28 @@ if __name__=='__main__':
     # 패키지명.모듈명.함수명() 형식으로 작성해야 함
     # my_collections.test_list.list_sample.make_list1() # 원하는 함수 실행(호출, call)시 import 한 모듈명.함수명() 으로 실행함
     # ls.make_list1() # 모듈 줄임말.함수명() 으로 사용함
-    make_list1() # 원하는 함수만 import 선언하면 함수명() 으로 실행함
-    make_list2()
+    
+    # make_list1() # 원하는 함수만 import 선언하면 함수명() 으로 실행함
+    # make_list2()
+    # list_indexing()
+    # list_slicing()
+    change_element()
     
     print('프로그램 종료 : main 이 끝남 ===========================')
+'''
+
+import my_collections.test_list.list_sample2 as ls2
+
+if __name__=='__main__':
+    print('program start ---------------------------------')
+
+    # 임포트한 모듈이 가진 전역변수 사용할 수 있음
+    # print(ls2.lst)
+    # ls2.list_append()
+    # ls2.list_remove()
+    # ls2.list_insert()
+    # ls2.list_pop()
+    # ls2.list_extend()
+    ls2.list_reverse()
+    
+    print('program exit ---------------------------------')
